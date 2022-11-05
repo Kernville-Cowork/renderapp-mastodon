9adffe64c1bc5a01206601c840d14317dff1e5e7
Justin Powers <justin@kernvillecowork.com>
2022-11-05 00:02:48 -0700

    simplify build script

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,7 +3,6 @@
 # exit on error
 set -o errexit
 
-bundle install
-bundle exec rake assets:precompile
-bundle exec rake assets:clean
+# bundle exec rake assets:precompile
+# bundle exec rake assets:clean
 bundle exec rake db:migrate
